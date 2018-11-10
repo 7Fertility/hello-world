5577e98f41267fee41e7d364cb1016f8d578899c
Aislinn <kyaula@gmail.com>
2018-11-10 20:52:02 +0000

    Changed font.

---
+++ kaneki7/Javascript.js
@@ -0,0 +1,11 @@
+var slideIndex= 0; 
+
+function showSlides (slideIndex){
+    var slides = document.getElementsByClassName("myimages");
+    for (var i = 0; i < slides.length; i ++) {
+        slides (i). style. display= "none";
+    }
+    slideIndex++;
+    
+    if (slideIndex >= slides.length)
+}
